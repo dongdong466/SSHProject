2ce287ae86f0a51dc9a1e8ef8da0bcf568c44c34
Administrator <1175497943@qq.com>
2018-04-14 16:02:48 +0800

    修改拦截器中session域在登录时存入user标识

--- src/com/dong1990/web/action/UserAction.java
+++ src/com/dong1990/web/action/UserAction.java
@@ -25,7 +25,7 @@ public class UserAction extends ActionSupport implements ModelDriven<User>{
         // 1.调用Service执行登录逻辑
         User u = userService.getUserByCodePassword(user);
         // 2.将返回的user对象放入session域
-        ActionContext.getContext().put("user",u);
+        ActionContext.getContext().getSession().put("user",u);
         // 3.重定向到项目首页
         return "toHome";
     }
